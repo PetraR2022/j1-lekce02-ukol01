00f45ccc597109d43af2d61d973c910f79620750
Petra Řezáčová <ing.rezacova@gmail.com>
2022-04-25 00:00:30 +0200

    Nakreslené jméno

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -75,9 +75,176 @@ public class HlavniProgram {
         zofka.move(80);
         zofka.penDown();
 
-            nakresliKolecko(zofka);
+        //nejdou mi paprsky???
 
+        nakresliKolecko(zofka);
 
+        zofka.penUp();
+        zofka.turnLeft(160);
+        zofka.move(300);
+
+        //pismeno P
+
+        nakresliP(zofka);
+
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnLeft(90);
+
+        //pismeno E
+
+        nakresliE(zofka);
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(50);
+        zofka.turnLeft(90);
+
+        //pismeno T
+
+        nakresliT(zofka);
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(50);
+        zofka.turnRight(90);
+
+        //pismeno R
+
+        nakresliR(zofka);
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(50);
+
+        //písmeno A
+        nakresliA(zofka);
+
+    }
+
+    private void nakresliA(Turtle zofka) {
+        zofka.turnLeft(70);
+        zofka.penDown();
+        zofka.move(60);
+        zofka.turnRight(130);
+        zofka.move(60);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(60);
+        zofka.penDown();
+        zofka.move(25);
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(25);
+        zofka.turnRight(50);
+        zofka.move(30);
+        zofka.turnRight(220);
+    }
+
+    private void nakresliR(Turtle zofka) {
+        zofka.turnRight(180);
+        zofka.penDown();
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.turnLeft(120);
+        zofka.move(35);
+        zofka.penUp();
+        zofka.turnLeft(150);
+    }
+
+    private void nakresliT(Turtle zofka) {
+        zofka.penDown();
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(20);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(20);
+        zofka.penDown();
+        zofka.move(20);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(20);
+        zofka.penDown();
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(60);
+        zofka.turnLeft(180);
+    }
+
+    private void nakresliE(Turtle zofka) {
+        zofka.penDown();
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(20);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(20);
+        zofka.turnLeft(90);
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.move(20);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(20);
+        zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.move(20);
+        zofka.turnLeft(90);
+    }
+
+    private void nakresliP(Turtle zofka) {
+        zofka.turnRight(180);
+        zofka.penDown();
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.move(5);
+        zofka.turnRight(20);
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnRight(180);
     }
 
     private void nakresliKolecko(Turtle zofka) {
